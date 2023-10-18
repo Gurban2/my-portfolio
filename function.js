513a29635dcea7b87692b97c9a938e090eabb4f2
Gurban2 <116964008+Gurban2@users.noreply.github.com>
2023-10-18 22:30:06 +0400

    Add files via upload

---
+++ function.js
@@ -0,0 +1,6 @@
+const instaProfile = document.getElementById("inst-profile");
+const instUrl = "https://www.instagram.com/muradovkq/";
+
+instaProfile.addEventListener("click", () => {
+  window.open(instUrl, "_blank");
+});
